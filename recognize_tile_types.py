927345c0f2ebdf59f30100b32b5b68fdcb441433
piotrbla <piotrb@gmail.com>
2016-01-18 12:21:44 +0000

    Small fixes

--- recognize_tile_types.py
+++ recognize_tile_types.py
@@ -32,20 +32,20 @@ def check_target_tiles_are_varied_non_original():
 def recognize_tile_types():
     check_result = TileCheckResult.NO_RESULT
     if check_dependencies_non_negative():
-        print 'All target tiles are fixed and the same as original ones because all elements of each distance vector ' \
-              'are non-negative'
+        print('All target tiles are fixed and the same as original ones because all elements of each distance vector '
+              'are non-negative')
         return TileCheckResult.NON_NEGATIVE
     if check_target_tiles_are_fixed():
-        print "All target tiles are fixed and the same as original ones because all iterations within each original " \
-              "tile are valid"
+        print('All target tiles are fixed and the same as original ones because all iterations within each original '
+              'tile are valid')
         return TileCheckResult.ARE_FIXED
     if check_target_tiles_are_fixed_non_original():
-        print "There exist fixed tiles different from original ones "
+        print('There exist fixed tiles different from original ones')
         check_result = TileCheckResult.FIXED_NON_ORIGINAL
     if check_target_tiles_are_varied_non_original():
-        print "There exist varied tiles different from original ones "
+        print('There exist varied tiles different from original ones')
         check_result += TileCheckResult.VARIED_NON_ORIGINAL
     if check_target_tiles_are_fixed():
-        print "There exist fixed parametric tiles"
+        print('There exist fixed parametric tiles')
         check_result += TileCheckResult.PARAMETRIC
     return check_result
